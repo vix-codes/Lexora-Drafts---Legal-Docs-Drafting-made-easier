b10cb158135ed6bd6a95a8d82b82fc4050241d1e
25MX356 - VIGNESH M <25mx356@psgtech.ac.in>
2025-11-12 17:17:06 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyC2d01i_...Hk",
+  apiKey: "AIzaSyDw-7iHgbZoBprqGPcqzZc2CtcBSz3TVvU",
   authDomain: "studio-9244104849-e5d65.firebaseapp.com",
   projectId: "studio-9244104849-e5d65",
   storageBucket: "studio-9244104849-e5d65.appspot.com",
-  messagingSenderId: "9244104849",
-  appId: "1:9244104849:web:35520119f029367b667822",
+  messagingSenderId: "638005660390",
+  appId: "1:638005660390:web:f51198ce0995f0d75fc927",
 };
