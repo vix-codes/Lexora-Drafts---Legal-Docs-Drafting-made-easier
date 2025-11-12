045d6ad60ef566a386a408c2571f95db68331b62
25MX356 - VIGNESH M <25mx356@psgtech.ac.in>
2025-11-12 18:17:51 +0000

    why not changing in browser tab top

---
+++ src/app/icon.tsx
@@ -0,0 +1,56 @@
+import { ImageResponse } from 'next/og'
+ 
+// Route segment config
+export const runtime = 'edge'
+ 
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+ 
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 24,
+          background: 'hsl(261 44% 11%)',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'hsl(262 80% 70%)',
+          borderRadius: '5px',
+        }}
+      >
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="m16 16-4-4-4 4"/>
+            <path d="M12 3v9"/>
+            <path d="M12 21a9 9 0 0 0 0-18 9 9 0 0 0 0 18Z"/>
+            <path d="M7 21a9 9 0 0 0 10 0"/>
+        </svg>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  )
+}
