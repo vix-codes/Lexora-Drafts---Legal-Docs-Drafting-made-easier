507914ca99afeadf742cc1d92b6cd0fcb21c798e
25MX356 - VIGNESH M <25mx356@psgtech.ac.in>
2025-11-12 16:11:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9244104849-e5d65",
+  "appId": "1:638005660390:web:f51198ce0995f0d75fc927",
+  "apiKey": "AIzaSyDw-7iHgbZoBprqGPcqzZc2CtcBSz3TVvU",
+  "authDomain": "studio-9244104849-e5d65.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "638005660390"
+};
