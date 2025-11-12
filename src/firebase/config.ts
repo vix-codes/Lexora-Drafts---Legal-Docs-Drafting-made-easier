68455e84e05e02200c494cb51ebec41481c46cba
25MX356 - VIGNESH M <25mx356@psgtech.ac.in>
2025-11-12 17:05:05 +0000

    see key error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyC2d01i_...Hk",
+  authDomain: "studio-9244104849-e5d65.firebaseapp.com",
+  projectId: "studio-9244104849-e5d65",
+  storageBucket: "studio-9244104849-e5d65.appspot.com",
+  messagingSenderId: "9244104849",
+  appId: "1:9244104849:web:35520119f029367b667822",
 };
