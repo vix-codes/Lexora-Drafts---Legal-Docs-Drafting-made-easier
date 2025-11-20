6669e1e5b5201d0efd7c1f903d026c21fc23038f
25MX356 - VIGNESH M <25mx356@psgtech.ac.in>
2025-11-20 10:15:24 +0000

    change website icon to this

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -26,23 +26,24 @@ export default function Icon() {
           justifyContent: 'center',
           color: 'hsl(262 80% 70%)',
           borderRadius: '5px',
+          filter: 'drop-shadow(0 0 3px hsl(262 80% 70%))',
         }}
       >
         <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
-            <path d="m16 16-4-4-4 4"/>
-            <path d="M12 3v9"/>
-            <path d="M12 21a9 9 0 0 0 0-18 9 9 0 0 0 0 18Z"/>
-            <path d="M7 21a9 9 0 0 0 10 0"/>
+          <path d="M12 3v18" />
+          <path d="M3 7h18" />
+          <path d="M5 7a5 5 0 0 1 5 5v2a5 5 0 0 1-10 0v-2a5 5 0 0 1 5-5Z" />
+          <path d="M19 7a5 5 0 0 1 5 5v2a5 5 0 0 1-10 0v-2a5 5 0 0 1 5-5Z" />
         </svg>
       </div>
     ),
