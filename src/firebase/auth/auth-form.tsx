8bc0cb388dec2a953280d35aa81a9a7cb0564c14
25MX356 - VIGNESH M <25mx356@psgtech.ac.in>
2025-12-04 17:15:32 +0000

    Try fixing this error: `Runtime Error: Error: Controller is not defined.

--- src/firebase/auth/auth-form.tsx
+++ src/firebase/auth/auth-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
