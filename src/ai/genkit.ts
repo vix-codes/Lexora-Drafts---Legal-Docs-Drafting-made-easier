a09af4a693085acff0d3486785ed55962215dd3d
25MX356 - VIGNESH M <25mx356@psgtech.ac.in>
2025-12-09 15:31:05 +0000

    WEF

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,5 +4,5 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
-  model: 'googleai/gemini-2.5-flash-live',
+  model: 'googleai/gemini-2.5-flash-lite',
 });
